23744a38a0556169d2e74e85077271f447f90ee4
yanMichellod <39124209+yanMichellod@users.noreply.github.com>
2019-04-11 09:41:14 +0200

    configure adc with timer1 interrupt

--- ide-sw4stm32/Oscilloscope/Src/isrs.cpp
+++ ide-sw4stm32/Oscilloscope/Src/isrs.cpp
@@ -17,6 +17,7 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 }
 
 void HAL_TIM_OC_DelayElapsedCallback(TIM_HandleTypeDef *htim){
+
 	HAL_GPIO_TogglePin(OutputTest_GPIO_Port, OutputTest_Pin);
 	//HAL_GPIO_WritePin(OutputTest_GPIO_Port, OutputTest_Pin,GPIO_PIN_RESET);
 }
